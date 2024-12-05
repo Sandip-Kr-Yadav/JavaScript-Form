1a9a74b19dad4103c5df34e84ffd8a6b4c322dfb
Sandip Kr Yadav <sandipyadav468@gmail.com>
2024-12-05 15:55:58 +0530

    Add delete function in Print div

--- form.js
+++ form.js
@@ -8,7 +8,7 @@ const genders=document.getElementsByName("gender");
 const data = document.getElementById("data");
 const print=document.getElementById("print")
 
-
+// Getting data of form elemrnt and storing values in the variable.
 document.getElementById("name").addEventListener('change',(e)=>{
     names=e.target.value;
 });
@@ -29,6 +29,7 @@ document.getElementById("phone_no").addEventListener('change',(e)=>{
 });
 
 
+//Console log of thr form input data.
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
     console.log(names);
@@ -38,13 +39,23 @@ submit.addEventListener('click',(e)=>{
     console.log(phone);
 });
 
+// dynamic way to create HTML element and add data in it.
 print.addEventListener('click',(e)=>{
-    data.innerHTML=`
+    let cur=document.createElement('div')
+    cur.innerHTML=`
     <b>Name=</b> ${names}<br>
     <b>Age=</b> ${age}<br>
     <b>Gender=</b> ${gender}<br>
     <b>Email=</b> ${mail}<br>
     <b>Phone number=</b> ${phone}<br>
-    `
-})
+    <button class="delete">Delete</button> `//Button to delete the div
+    
+    data.appendChild(cur);
+
+    const deleteButton = cur.querySelector('.delete'); // function to delete the div
+    deleteButton.addEventListener('click',()=>{
+        cur.remove();
+    });
+
+});
 console.log(data);
